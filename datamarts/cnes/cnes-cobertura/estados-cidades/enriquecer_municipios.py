a9c0bf7cd6907aef2ef134f581debe004c3e1992
Gleyton Lima <gleytonclima@gmail.com>
2023-03-07 22:56:17 -0400

    Ajustando dashboard e adicionando populacao em municipio

--- datamarts/cnes/cnes-cobertura/estados-cidades/enriquecer_municipios.py
+++ datamarts/cnes/cnes-cobertura/estados-cidades/enriquecer_municipios.py
@@ -6,6 +6,7 @@ def enriquecer_municipios():
     df_municipios = pd.read_csv("municipios-originais.csv")
     df_estados = pd.read_csv("estados.csv")
     df_estados = df_estados.rename(columns={"uf": "UF", "nome": "munResUf"})
+    df_populacao_municipio = pd.read_csv("../populacao/POP2022_Municipios.csv", dtype={"CODUFMUN": object})
     df_municipios['codigo_ibge'] = df_municipios['codigo_ibge'].astype(str).str[:-1]
     df_municipios = df_municipios.rename(columns={'codigo_ibge': 'CODUFMUN'})
 
@@ -33,6 +34,10 @@ def enriquecer_municipios():
                                  ['CODUFMUN', "UF", 'COD_REGIAO_SAUDE', 'NOME_REGIAO_SAUDE', 'munResUf']],
                              on='CODUFMUN',
                              how='left')
+    df_municipios = pd.merge(df_municipios,
+                             df_populacao_municipio[["CODUFMUN", "POPULACAO"]],
+                             on="CODUFMUN",
+                             how="left")
     # df_municipios['nome'] = df_municipios['nome'] + ', ' + df_municipios['munResUf'] + ', Brasil'
     # Escreva o dataframe no arquivo csv
     df_municipios.to_csv('municipios.csv', index=False)
