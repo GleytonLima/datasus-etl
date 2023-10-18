ae46fa602e8473e6c7b33db1643f82c1ef12ef8a
Gleyton Lima <gleytonclima@gmail.com>
2023-10-18 00:06:43 -0400

    ajustando servicos ambulatoriais

--- etl/scripts/transform_cnes_sr_sap.py
+++ etl/scripts/transform_cnes_sr_sap.py
@@ -4,8 +4,8 @@ transformar_cnes_sr = TransformarCnesSR(
     nome_arquivo_saida="cnes-sr-numero-servicos-ambulatoriais-em-psiquiatria.csv",
     nome_totalizador="TOTAL_SERVICO_AMBULATORIAL_EM_PSIQUIATRIA",
     codigos_servico_especializado=[115],
-    codigos_tipo_unidade=[],
-    codigos_classificacao=[4, 36]
+    codigos_tipo_unidade=[4, 36],
+    codigos_classificacao=[2]
 )
 
 transformar_cnes_sr.calcular_quantitativo_sr()
